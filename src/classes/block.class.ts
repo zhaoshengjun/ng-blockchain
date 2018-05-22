618a5e3f0170e1b887ae4f0638f34d05298f6822
Sam Zhao <zhao.shengjun@gmail.com>
2018-05-22 23:02:51 +1000

    create block class

---
+++ src/classes/block.class.ts
@@ -0,0 +1,23 @@
+import * as SHA256 from "crypto-js/sha256";
+
+export class Block {
+  hash: string = null;
+
+  constructor(
+    public index: number,
+    public timestamp: any,
+    public data: any,
+    public previousHash: string = null
+  ) {
+    this.hash = this.calculateHash();
+  }
+
+  private calculateHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data)
+    ).toString();
+  }
+}
