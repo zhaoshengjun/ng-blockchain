51f8d3c2dced74d652495161c71db60b1eb86b1a
Sam Zhao <zhao.shengjun@gmail.com>
2018-05-23 21:47:46 +1000

    display blockchain in html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { CryptoService } from "./services/crypto.service";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "app";
+  blockchain = "";
+  constructor(private cryptoSvc: CryptoService) {
+    this.blockchain = JSON.stringify(this.cryptoSvc.cyptoChain);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { CryptoService } from "./services/crypto.service";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [CryptoService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
