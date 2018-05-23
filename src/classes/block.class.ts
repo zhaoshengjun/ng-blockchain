6402a9f871a90ff0f9f272c096038d0340ace7a9
Sam Zhao <zhao.shengjun@gmail.com>
2018-05-23 22:12:11 +1000

    mine block

--- src/classes/block.class.ts
+++ src/classes/block.class.ts
@@ -2,6 +2,7 @@ import * as SHA256 from "crypto-js/sha256";
 
 export class Block {
   hash: string = null;
+  nonce: number = 0;
 
   constructor(
     public index: number,
@@ -17,7 +18,27 @@ export class Block {
       this.index +
         this.previousHash +
         this.timestamp +
-        JSON.stringify(this.data)
+        JSON.stringify(this.data) +
+        this.nonce
     ).toString();
+  }
+
+  mineBlock(difficulty: number): Promise<any> {
+    let promise = new Promise((resolve, reject) => {
+      while (
+        this.hash.substring(0, difficulty) != Array(difficulty + 1).join(0)
+      ) {
+        this.nonce++;
+        this.hash = this.calculateHash();
+      }
+      console.log(
+        `Block successfully hashed ( ${this.nonce} iterations. Hash: ${
+          this.hash
+        }) `
+      );
+      resolve();
+    });
+
+    return promise;
   }
 }
