c3591657e0318786ce93a88f90828f88f2a033f6
Sam Zhao <zhao.shengjun@gmail.com>
2018-05-24 23:00:40 +1000

    use angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { CryptoService } from "./services/crypto.service";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MyMaterialModule } from './modules/my-material/my-material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MyMaterialModule],
   providers: [CryptoService],
   bootstrap: [AppComponent]
 })
