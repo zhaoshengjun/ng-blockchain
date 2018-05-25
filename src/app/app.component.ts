4644cdb993cadc4b187ce1bcd5c38516e2ab9a16
Sam Zhao <zhao.shengjun@gmail.com>
2018-05-25 22:07:46 +1000

    use Wallet class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Wallet } from "./../classes/wallet.class";
 import { CryptoService } from "./services/crypto.service";
 import { Component } from "@angular/core";
 import { FormGroup, Validators, FormBuilder } from "@angular/forms";
@@ -12,6 +13,7 @@ export class AppComponent {
   blockchain = "";
   isChainValid: boolean = true;
   balance: number = 0;
+  wallet: Wallet;
 
   constructor(private cryptoSvc: CryptoService, private fb: FormBuilder) {
     this.walletAddressForm = this.fb.group({
@@ -19,9 +21,9 @@ export class AppComponent {
     });
     this.blockchain = JSON.stringify(this.cryptoSvc.cryptoChain);
     this.isChainValid = this.cryptoSvc.cryptoChain.isChainValid();
+    this.wallet = new Wallet();
   }
-
   assignWalletAddress() {
-    console.log("called assignWalletAddress");
+    this.wallet.assignWalletAddress(this.walletAddressForm.value.walletAddress);
   }
 }
--- src/classes/wallet.class.ts
+++ src/classes/wallet.class.ts
@@ -1,4 +1,9 @@
 export class Wallet {
   address: string = "";
   balance: number = 0;
+
+  assignWalletAddress(address: string) {
+    this.address = address;
+    console.log(`Assigned wallet this address: ${address}`);
+  }
 }
