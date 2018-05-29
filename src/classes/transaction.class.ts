4e5e871004cc50010bd8e33953d323b6873d1832
Sam Zhao <zhao.shengjun@gmail.com>
2018-05-29 21:30:19 +1000

    add CD class to transaction

--- src/classes/transaction.class.ts
+++ src/classes/transaction.class.ts
@@ -1,8 +1,10 @@
+import { CD } from "./cd.class";
 export class Transaction {
   constructor(
     public timestamp,
     public payerAddr: string,
     public payeeAddr: string,
-    public amount: number = 0
+    public amount: number = 0,
+    public smartContract: CD = null
   ) {}
 }
